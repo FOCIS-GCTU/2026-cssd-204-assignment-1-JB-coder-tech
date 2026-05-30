4b1d76f85cd8db43dae9cfaed4d296fa1c0a3282
Judy ✨️ <judithbleboo0@gmail.com>
2026-05-30 04:39:05 +0000

    Assignment 1 submission

--- initials.py
+++ initials.py
@@ -1,6 +1,34 @@
+# File: initials.py
+# Description: Printing out my initials.
+# Assignment Number: 1
+#
+# Name: Judith Enyo Afi Bleboo
+# STUDENT ID: 2425401813 
+# Email: 2425401813@gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Judith Enyo Afi Bleboo, this programming assignment is my own work
+# and I have not provided this code to any other student. 
 
-def main():
-  pass #code goes here
+def main():  
+     #code goes here
+   
+   #print my initial large styled letters using J.E.B. 
+  print("""  
+         
+    ...JEB  
+
+        ...JJJJJJJJJJJJ........EEEEEEEEEEEE........BBBBBBBBBB.......
+        ...JJJJJJJJJJJJ........EEEEEEEEEEEE........BBBBBBBBBBBB..... 
+        ............JJJ........EEE.................BBB......BBB..... 
+        ............JJJ........EEE.................BBB......BBB..... 
+        ............JJJ........EEEEEEEEEEEE........BBBBBBBBBB....... 
+        ............JJJ........EEEEEEEEEEEE........BBBBBBBBBB....... 
+        ............JJJ........EEE.................BBB......BBB..... 
+        ...JJJ......JJJ........EEE.................BBB......BBB..... 
+        ....JJJJ...JJJJ...**...EEEEEEEEEEEE...**...BBBBBBBBBBBB...** 
+        ......JJJJJJJ.....**...EEEEEEEEEEEE...**...BBBBBBBBBB.....**  
+       """ )
 
 
 
